d5bd2d48a423d58bf0a9d6df026b27e7d4989e95
YJ <angyujuan.2001@gmail.com>
2025-01-30 18:55:04 +0800

    feat: flask_restx for swagger documentation

--- api/api.py
+++ api/api.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+
 import time
+from flask_restx import Resource, Api, fields
+from flask_cors import CORS
+from flask import Flask, request, jsonify
 
 import sys
 import os
@@ -10,109 +12,132 @@ from algo.algorithms.algo import MazeSolver  # nopep8
 from algo.tools.commands import CommandGenerator  # nopep8
 
 app = Flask(__name__)
+api = Api(app)
+
 CORS(app)
 # model = load_model()
 model = None
 
 
-@app.route('/status', methods=['GET'])
-def status():
-    """
-    This is a health check endpoint to check if the server is running
-    :return: a json object with a key "result" and value "ok"
-    """
-    return jsonify({"result": "ok"})
+@api.route('/status')
+class Status(Resource):
+    def get(self):
+        """
+        This is a health check endpoint to check if the server is running
+        :return: a json object with a key "result" and value "ok"
+        """
+        return jsonify({"result": "ok"})
 
 
-@app.route('/path', methods=['POST'])
-def path_finding():
-    """
-    This is the main endpoint for the path finding algorithm
-    :return: a json object with a key "data" and value a dictionary with keys "distance", "path", and "commands"
-    """
-    # Get the json data from the request
-    content = request.json
+obstacle = api.model('Obstacle', {
+    'x': fields.Integer,
+    'y': fields.Integer,
+    'd': fields.Integer,
+    'id': fields.Integer
+})
 
-    # Get the obstacles, big_turn, retrying, robot_x, robot_y, and robot_direction from the json data
-    obstacles = content['obstacles']
-    # big_turn = int(content['big_turn'])
-    retrying = content['retrying']
-    robot_x, robot_y = content['robot_x'], content['robot_y']
-    robot_direction = int(content['robot_dir'])
-
-    # Initialize MazeSolver object with robot size of 20x20, bottom left corner of robot at (1,1), facing north, and whether to use a big turn or not.
-    maze_solver = MazeSolver(size_x=20, size_y=20, robot_x=robot_x,
-                             robot_y=robot_y, robot_direction=robot_direction, big_turn=1)
-    # Add each obstacle into the MazeSolver. Each obstacle is defined by its x,y positions, its direction, and its id
-    for ob in obstacles:
-        maze_solver.add_obstacle(ob['x'], ob['y'], ob['d'], ob['id'])
-
-    start = time.time()
-    # Get shortest path
-    optimal_path, cost = maze_solver.get_optimal_path(retrying=retrying)
-    print(
-        f"Time taken to find shortest path using A* search: {time.time() - start}s")
-    print(f"cost to travel: {cost} units")
-
-    # Based on the shortest path, generate commands for the robot
-    motions = maze_solver.optimal_path_to_motion_path(optimal_path)
-    command_generator = CommandGenerator()
-    commands = command_generator.generate_commands(motions, testing=False)
-
-    # Get the starting location and add it to path_results
-    path_results = [optimal_path[0].get_dict()]
-    for pos in optimal_path:
-        path_results.append(pos.get_dict())
-
-    return jsonify({
-        "data": {
-            'distance': cost,
-            'path': path_results,
-            'commands': commands
-        },
-        "error": None
-    })
+path_finding_request = api.model('PathFindingRequest', {
+    'obstacles': fields.List(fields.Nested(obstacle)),
+    'retrying': fields.Boolean,
+    'big_turn': fields.Integer,
+    'robot_dir': fields.Integer,
+    'robot_x': fields.Integer,
+    'robot_y': fields.Integer
+})
 
 
-@app.route('/image', methods=['POST'])
-def image_predict():
-    """
-    This is the main endpoint for the image prediction algorithm
-    :return: a json object with a key "result" and value a dictionary with keys "obstacle_id" and "image_id"
-    """
-    file = request.files['file']
-    filename = file.filename
-    file.save(os.path.join('uploads', filename))
-    # filename format: "<timestamp>_<obstacle_id>_<signal>.jpeg"
-    constituents = file.filename.split("_")
-    obstacle_id = constituents[1]
+@api.route('/path')
+class PathFinding(Resource):
+    @api.expect(path_finding_request)
+    def post(self):
+        """
+        This is the main endpoint for the path finding algorithm
+        :return: a json object with a key "data" and value a dictionary with keys "distance", "path", and "commands"
+        """
+        # Get the json data from the request
+        content = request.json
 
-    ## Week 8 ##
-    # signal = constituents[2].strip(".jpg")
-    # image_id = predict_image(filename, model, signal)
+        # Get the obstacles, big_turn, retrying, robot_x, robot_y, and robot_direction from the json data
+        obstacles = content['obstacles']
+        big_turn = content.get('big_turn', 1)
+        retrying = content['retrying']
+        robot_x, robot_y = content['robot_x'], content['robot_y']
+        robot_direction = content['robot_dir']
+        # Initialize MazeSolver object with robot size of 20x20, bottom left corner of robot at (1,1), facing north, and whether to use a big turn or not.
+        maze_solver = MazeSolver(size_x=20, size_y=20, robot_x=robot_x,
+                                 robot_y=robot_y, robot_direction=robot_direction, big_turn=big_turn)
+        # Add each obstacle into the MazeSolver. Each obstacle is defined by its x,y positions, its direction, and its id
+        for ob in obstacles:
+            maze_solver.add_obstacle(ob['x'], ob['y'], ob['d'], ob['id'])
 
-    ## Week 9 ##
-    # We don't need to pass in the signal anymore
-    image_id = predict_image_week_9(filename, model)
+        start = time.time()
+        # Get shortest path
+        optimal_path, cost = maze_solver.get_optimal_path(retrying=retrying)
+        print(
+            f"Time taken to find shortest path using A* search: {time.time() - start}s")
+        print(f"cost to travel: {cost} units")
 
-    # Return the obstacle_id and image_id
-    result = {
-        "obstacle_id": obstacle_id,
-        "image_id": image_id
-    }
-    return jsonify(result)
+        # Based on the shortest path, generate commands for the robot
+        motions = maze_solver.optimal_path_to_motion_path(optimal_path)
+        command_generator = CommandGenerator()
+        commands = command_generator.generate_commands(motions)
+
+        # Get the starting location and add it to path_results
+        path_results = [optimal_path[0].get_dict()]
+        for pos in optimal_path:
+            path_results.append(pos.get_dict())
+
+        return jsonify({
+            "data": {
+                'distance': cost,
+                'path': path_results,
+                'commands': commands
+            },
+            "error": None
+        })
 
 
-@app.route('/stitch', methods=['GET'])
-def stitch():
-    """
-    This is the main endpoint for the stitching command. Stitches the images using two different functions, in effect creating two stitches, just for redundancy purposes
-    """
-    img = stitch_image()
-    img.show()
-    img2 = stitch_image_own()
-    img2.show()
-    return jsonify({"result": "ok"})
+@api.route('/image')
+class ImagePredict(Resource):
+    def post():
+        """
+        This is the main endpoint for the image prediction algorithm
+        :return: a json object with a key "result" and value a dictionary with keys "obstacle_id" and "image_id"
+        """
+        file = request.files['file']
+        filename = file.filename
+        file.save(os.path.join('uploads', filename))
+        # filename format: "<timestamp>_<obstacle_id>_<signal>.jpeg"
+        constituents = file.filename.split("_")
+        obstacle_id = constituents[1]
+
+        ## Week 8 ##
+        # signal = constituents[2].strip(".jpg")
+        # image_id = predict_image(filename, model, signal)
+
+        ## Week 9 ##
+        # We don't need to pass in the signal anymore
+        image_id = predict_image_week_9(filename, model)
+
+        # Return the obstacle_id and image_id
+        result = {
+            "obstacle_id": obstacle_id,
+            "image_id": image_id
+        }
+        return jsonify(result)
+
+
+@api.route('/stitch')
+class Stitch(Resource):
+    def get(self):
+        """
+        This is the main endpoint for the stitching command. Stitches the images using two different functions, in effect creating two stitches, just for redundancy purposes
+        """
+        img = stitch_image()
+        img.show()
+        img2 = stitch_image_own()
+        img2.show()
+        return jsonify({"result": "ok"})
 
 
 if __name__ == '__main__':
