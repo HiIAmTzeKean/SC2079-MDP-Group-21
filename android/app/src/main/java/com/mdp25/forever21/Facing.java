a98f15010111503a7e69fb20f11de81009c9dcb1
Terence Sim <40583743+InTheAxis@users.noreply.github.com>
2025-02-04 02:19:42 +0800

    added mappings to facings

--- android/app/src/main/java/com/mdp25/forever21/Facing.java
+++ android/app/src/main/java/com/mdp25/forever21/Facing.java
@@ -1,8 +1,25 @@
 package com.mdp25.forever21;
 
 /**
- * Simple enum for heading/direction/facing
+ * Simple enum for heading/direction/facing.
  */
 public enum Facing {
-    NORTH, SOUTH, EAST, WEST
+    NORTH(0),
+    EAST(2),
+    SOUTH(4),
+    WEST(6),
+    SKIP(8);
+
+    private int mappedCode;
+
+    Facing(int code) {
+        mappedCode = code;
+    }
+
+    /**
+     * @return the mapped integer to represent the direction
+     */
+    public int getMappedCode() {
+        return mappedCode;
+    }
 }
