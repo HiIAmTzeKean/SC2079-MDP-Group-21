920412040530a0f86188f788bfdeb6f368132a61
HiIAmTzeKean <ngtzekean@gmail.com>
2025-02-03 21:56:52 +0800

    feat: constants to lock in values

--- RPI_Server-main/constant/consts.py
+++ RPI_Server-main/constant/consts.py
@@ -1,3 +1,6 @@
+from enum import Enum
+
+
 SYMBOL_MAP = {
     "10": "Bullseye",
     "11": "One",
@@ -30,4 +33,26 @@ SYMBOL_MAP = {
     "38": "Right Arrow",
     "39": "Left Arrow",
     "40": "Stop"
+}
+
+class Category(Enum):
+    INFO = 'info'
+    MODE = 'mode' # the current mode of the robot (`manual` or `path`)
+    PATH = 'path'
+    SNAP = 'snap'
+    LOCATION = 'location' # the current location of the robot (in Path mode)
+    FAILED = 'failed'
+    SUCCESS = 'success'
+    ERROR = 'error'
+    OBSTACLE = 'obstacle' # list of obstacles
+    IMAGE_REC = 'image-rec' # image recognition results
+    STATUS = 'status' # status updates of the robot (`running` or `finished`)
+
+manual_commands = {
+    "front": ("T", 50, 0, 10),
+    "back": ("t", 50, 0, 10),
+    "left": ("T", 50, -25, 90),
+    "right": ("T", 50, 25, 90),
+    "bright": ("t", 30, 20, 90),
+    "bleft": ("t", 30, -20, 90)
 }
