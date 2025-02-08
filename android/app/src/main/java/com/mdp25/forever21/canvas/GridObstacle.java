99064728be5ebdad78d404ad12a5abd3f118aeb9
Ian Koh Jin <149784365+IKJ9@users.noreply.github.com>
2025-02-08 17:49:00 +0800

    Update GridObstacle.java

--- android/app/src/main/java/com/mdp25/forever21/canvas/GridObstacle.java
+++ android/app/src/main/java/com/mdp25/forever21/canvas/GridObstacle.java
@@ -1,22 +1,29 @@
 package com.mdp25.forever21.canvas;
 
 import com.mdp25.forever21.Facing;
-import com.mdp25.forever21.Position;
+import com.mdp25.forever21.Target;
 
 /**
  * Represents a obstacle that can be placed on the grid.
- * <p> Initially, there is only a facing and ID.
- * <p> Once the robot recognises the number, the relevant (nearest?) obstacle's number is updated.
+ * <p> Initially, there is only a Facing, ID, and Target.
+ * <p> Default facing is set to NORTH and target to null.
  */
 public class GridObstacle {
-    private final Facing facing;
-    private final int id; // i.e. which nth obstacle is this?
+    private final int id; // id of obstacle
+    private Facing facing;
+    private Target target;
 
-    // some data too store position?
-    private Position position;
-
-    public GridObstacle(Facing facing, int id) {
-        this.facing = facing;
+    public GridObstacle(int id) {
+        this.facing = Facing.NORTH;
         this.id = id;
+        this.target = null;
+    }
+
+    public void updateFacing(Facing facing){
+        this.facing = facing;
+    }
+
+    public void updateTarget(Target target){
+        this.target = target;
     }
 }
