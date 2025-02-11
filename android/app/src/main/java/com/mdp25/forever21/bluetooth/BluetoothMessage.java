f97a61f0b8e8bbabaea58711b0d9d5be01f09c13
Terence Sim <40583743+InTheAxis@users.noreply.github.com>
2025-02-11 17:30:41 +0800

    aligned messages with rpi side

--- android/app/src/main/java/com/mdp25/forever21/bluetooth/BluetoothMessage.java
+++ android/app/src/main/java/com/mdp25/forever21/bluetooth/BluetoothMessage.java
@@ -1,40 +1,112 @@
 package com.mdp25.forever21.bluetooth;
 
-import android.bluetooth.BluetoothDevice;
-import android.content.Intent;
-import android.os.Parcel;
-import android.os.Parcelable;
+import android.util.Log;
 
-import androidx.annotation.NonNull;
+import com.mdp25.forever21.canvas.GridObstacle;
 
-import java.util.Objects;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
 
 /**
  * Represents a bluetooth message sent FROM the robot.
  * <p> Essentially wraps around a raw message string and provides the parsed info as getters.
  * <p> Use {@code ofXXX()} to create the subclassed record.
  */
-public sealed interface BluetoothMessage permits BluetoothMessage.CustomMessage, BluetoothMessage.PlainStringMessage, BluetoothMessage.RobotPositionMessage, BluetoothMessage.RobotStatusMessage, BluetoothMessage.TargetFoundMessage {
+public sealed interface BluetoothMessage permits BluetoothMessage.CustomMessage, BluetoothMessage.ObstaclesMessage, BluetoothMessage.PlainStringMessage, BluetoothMessage.RobotMoveMessage, BluetoothMessage.RobotPositionMessage, BluetoothMessage.RobotStartMessage, BluetoothMessage.RobotStatusMessage, BluetoothMessage.TargetFoundMessage {
     // this class uses the sealed..permit feature as a usage example, not strictly necessary
 
+    public static final String TAG = "BluetoothMessage";
+
+    /**
+     * Received from RPI.
+     */
     public record RobotStatusMessage(String rawMsg, String status) implements BluetoothMessage {}
     public static BluetoothMessage ofRobotStatusMessage(String rawMsg, String status) {
         return new RobotStatusMessage(rawMsg, status);
     }
 
+    /**
+     * Received from RPI.
+     */
     public record TargetFoundMessage(String rawMsg, int obstacleId, int targetId) implements BluetoothMessage {}
     public static BluetoothMessage ofTargetFoundMessage(String rawMsg, int obstacleId, int targetId) {
         return new TargetFoundMessage(rawMsg, obstacleId, targetId);
     }
 
+    /**
+     * Received from RPI.
+     */
     public record RobotPositionMessage(String rawMsg, int x, int y, int direction) implements BluetoothMessage {}
     public static BluetoothMessage ofRobotPositionMessage(String rawMsg, int x, int y, int direction) {
         return new RobotPositionMessage(rawMsg, x, y, direction);
     }
 
+    /**
+     * Any plain old string message from RPI.
+     */
     public record PlainStringMessage(String rawMsg) implements BluetoothMessage {}
     public static BluetoothMessage ofPlainStringMessage(String rawMsg) {
         return new PlainStringMessage(rawMsg);
+    }
+
+    /**
+     * Sent to RPI.
+     */
+    public record RobotMoveMessage(RobotMoveCommand cmd) implements BluetoothMessage, JsonMessage {
+        @Override
+        public String getAsJson() {
+            return getFormatted("control", cmd.value());
+        }
+    }
+    public static BluetoothMessage ofRobotMoveMessage(RobotMoveCommand cmd) {
+        return new RobotMoveMessage(cmd);
+    }
+
+    /**
+     * Sent to RPI.
+     */
+    public record RobotStartMessage() implements BluetoothMessage, JsonMessage {
+        @Override
+        public String getAsJson() {
+            return getFormatted("control", "start");
+        }
+    }
+    public static BluetoothMessage ofRobotStartMessage() {
+        return new RobotStartMessage();
+    }
+
+    /**
+     * Sent to RPI.
+     * TODO
+     */
+    public record ObstaclesMessage(List<GridObstacle> obstacleList) implements BluetoothMessage, JsonMessage {
+        @Override
+        public String getAsJson() {
+            JSONObject jsonObject = new JSONObject();
+            try {
+                jsonObject.put("mode", "0");
+                JSONArray arr = new JSONArray();
+                for (GridObstacle obst : obstacleList) {
+                    JSONObject obstJson = new JSONObject();
+//                    obstJson.put("x", obst.); // TODO waiting on obstacles class...
+//                    obstJson.put("y", obst.);
+//                    obstJson.put("id", obst.);
+//                    obstJson.put("d", obst.);
+                    arr.put(obstJson);
+                }
+                jsonObject.put("obstacles", arr);
+            } catch (JSONException e) {
+                Log.e(TAG,"Error creating json for ObstaclesMessage");
+                throw new RuntimeException(e);
+            }
+            return getFormatted("obstacles", jsonObject.toString());
+        }
+    }
+    public static BluetoothMessage ofObstaclesMessage(List<GridObstacle> obstacleList) {
+        return new ObstaclesMessage(obstacleList);
     }
 
     /**
--- android/app/src/main/java/com/mdp25/forever21/bluetooth/BluetoothMessageParser.java
+++ android/app/src/main/java/com/mdp25/forever21/bluetooth/BluetoothMessageParser.java
@@ -1,5 +1,7 @@
 package com.mdp25.forever21.bluetooth;
 
+import android.util.Log;
+
 import java.util.function.Function;
 
 /**
@@ -7,22 +9,64 @@ import java.util.function.Function;
  */
 public interface BluetoothMessageParser extends Function<String, BluetoothMessage> {
 
-    // note no error checking is done, assume message is valid
-    // technically not a secure approach, but fine for this project
+    public static final String TAG = "BluetoothMessageParser";
 
+    /**
+     * Default message format is {@code "command;param1;param2" }.
+     * <p> Expected messages:
+     * <ul>
+     *     <li>info</li>
+     *     <li>error</li>
+     *     <li>mode</li>
+     *     <li>status</li>
+     *     <li>location</li>
+     *     <li>image-rec</li>
+     * </ul>
+     */
     BluetoothMessageParser DEFAULT = msg -> {
-        String[] split = msg.split(",");
-        if (split.length > 1) {
-            String pattern = split[0];
-            return switch (pattern) {
-                case "STATUS" -> BluetoothMessage.ofRobotStatusMessage(msg, split[1]);
-                case "TARGET" -> BluetoothMessage.ofTargetFoundMessage(msg, Integer.parseInt(split[1]), Integer.parseInt(split[2]));
-                case "ROBOT" -> BluetoothMessage.ofRobotPositionMessage(msg, Integer.parseInt(split[1]), Integer.parseInt(split[2]), Integer.parseInt(split[3])); //TODO
-                default -> BluetoothMessage.ofPlainStringMessage(msg);
-            };
+        String[] params = msg.split(";");
+        if (params.length > 1) {
+            String command = params[0];
+            BluetoothMessage ret;
+            switch (command) {
+                case "info" -> ret = BluetoothMessage.ofPlainStringMessage("[info] " + params[1]);
+                case "error" -> ret = BluetoothMessage.ofPlainStringMessage("[error] " + params[1]);
+                case "mode" -> ret = BluetoothMessage.ofPlainStringMessage("[mode] " + params[1]);
+                case "status" -> ret = BluetoothMessage.ofRobotStatusMessage(msg, params[1]);
+                case "location" -> {
+                    int[] intParams = tryGetIntParams(params, 3);
+                    ret = BluetoothMessage.ofRobotPositionMessage(msg, intParams[0], intParams[1], intParams[2]);
+                }
+                case "image-rec" -> {
+                    int[] intParams = tryGetIntParams(params, 2);
+                    ret = BluetoothMessage.ofTargetFoundMessage(msg, intParams[0], intParams[1]);
+                }
+                default -> ret = BluetoothMessage.ofPlainStringMessage(msg);
+            }
+            ;
+            return ret;
         }
         return BluetoothMessage.ofPlainStringMessage(msg);
     };
+
+    public static int[] tryGetIntParams(String[] params, int expectedSize) {
+        int[] ret = new int[expectedSize];
+        // if input not valid just return empty list
+        if (params.length <= expectedSize) {
+            Log.e(TAG, String.format("Error in params, expected %d but size was %d", expectedSize, params.length));
+            return ret;
+        }
+        // start from params index 1, since 0 is command
+        for (int i = 0; i < expectedSize; ++i) {
+            try {
+                ret[i] = Integer.parseInt(params[i + 1]);
+            } catch (NumberFormatException e) {
+                ret[i] = 0; //put a safe non-crash value
+                Log.e(TAG, "Error in parsing " + params[i + 1]);
+            }
+        }
+        return ret;
+    }
 
     public static BluetoothMessageParser ofDefault() {
         return DEFAULT;
