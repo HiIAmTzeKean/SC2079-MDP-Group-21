543087c17e0b71e9928ab5d67342353a38dbc844
kakapoot <angyujuan.2001@gmail.com>
2025-02-13 00:37:00 +0800

    test: another 7 obstacles test

--- algo/simulator/src/tests/algorithm/index.ts
+++ algo/simulator/src/tests/algorithm/index.ts
@@ -4,12 +4,12 @@ import { AlgoTestBasicUTurn } from "./basic_u_turn";
 import { AlgoTestCorners } from "./corners";
 import { AlgoTestCustom } from "./custom";
 import { AlgoTestObstacles_5_Basic } from "./obstacles_5";
-import { AlgoTestObstacles_7 } from "./obstacles_7";
+import { AlgoTestObstacles_7_A, AlgoTestObstacles_7_B } from "./obstacles_7";
 import { AlgoTestShapes_V } from "./shapes";
 import {
-  AlgoTestCollisionCheck_A,
-  AlgoTestCollisionCheck_B,
-  AlgoTestCollisionCheck_C,
+	AlgoTestCollisionCheck_A,
+	AlgoTestCollisionCheck_B,
+	AlgoTestCollisionCheck_C,
 } from "./collision_check";
 import { AlgoTestOfficialMockLayout } from "./official_mock_layout";
 
@@ -17,62 +17,50 @@ import { AlgoTestOfficialMockLayout } from "./official_mock_layout";
  * @param obstacles An array of Obstacles.
  */
 export interface AlgoTestDataInterface {
-  obstacles: Obstacle[];
+	obstacles: Obstacle[];
 }
 
 export enum AlgoTestEnum {
-  Custom = "Custom",
-  Basic_Mock = "Basic Mock",
-  Basic_U_Turn = "Basic U-Turn",
-  Corners = "Corners",
-  Obstacles_7 = "7 Obstacles",
-  Shapes_V = "V Shape",
-  Obstacles_5_Basic = "5 Obstacles (Basic)",
-  AlgoTestCollisionCheck_A = "Collision Checking (A)",
-  AlgoTestCollisionCheck_B = "Collision Checking (B)",
-  AlgoTestCollisionCheck_C = "Collision Checking (C)",
-  AlgoTestOfficialMockLayout = "Official Mock Layout",
+	Custom = "Custom",
+	Basic_Mock = "Basic Mock",
+	Basic_U_Turn = "Basic U-Turn",
+	Corners = "Corners",
+	Obstacles_7_A = "7 Obstacles (A)",
+	Obstacles_7_B = "7 Obstacles (B)",
+	Shapes_V = "V Shape",
+	Obstacles_5_Basic = "5 Obstacles (Basic)",
+	AlgoTestCollisionCheck_A = "Collision Checking (A)",
+	AlgoTestCollisionCheck_B = "Collision Checking (B)",
+	AlgoTestCollisionCheck_C = "Collision Checking (C)",
+	AlgoTestOfficialMockLayout = "Official Mock Layout",
 }
 
 export const AlgoTestEnumsList = [
-  AlgoTestEnum.Custom,
-  AlgoTestEnum.Basic_Mock,
-  AlgoTestEnum.Basic_U_Turn,
-  AlgoTestEnum.Corners,
-  AlgoTestEnum.Obstacles_7,
-  AlgoTestEnum.Shapes_V,
-  AlgoTestEnum.Obstacles_5_Basic,
-  AlgoTestEnum.AlgoTestCollisionCheck_A,
-  AlgoTestEnum.AlgoTestCollisionCheck_B,
-  AlgoTestEnum.AlgoTestCollisionCheck_C,
-  AlgoTestEnum.AlgoTestOfficialMockLayout,
+	AlgoTestEnum.Custom,
+	AlgoTestEnum.Basic_Mock,
+	AlgoTestEnum.Basic_U_Turn,
+	AlgoTestEnum.Corners,
+	AlgoTestEnum.Obstacles_7_A,
+	AlgoTestEnum.Obstacles_7_B,
+	AlgoTestEnum.Shapes_V,
+	AlgoTestEnum.Obstacles_5_Basic,
+	AlgoTestEnum.AlgoTestCollisionCheck_A,
+	AlgoTestEnum.AlgoTestCollisionCheck_B,
+	AlgoTestEnum.AlgoTestCollisionCheck_C,
+	AlgoTestEnum.AlgoTestOfficialMockLayout,
 ];
 
 export const AlgoTestEnumMapper = {
-  [AlgoTestEnum.Custom]: AlgoTestCustom,
-  [AlgoTestEnum.Basic_Mock]: AlgoTestBasicMock,
-  [AlgoTestEnum.Basic_U_Turn]: AlgoTestBasicUTurn,
-  [AlgoTestEnum.Corners]: AlgoTestCorners,
-  [AlgoTestEnum.Obstacles_7]: AlgoTestObstacles_7,
-  [AlgoTestEnum.Shapes_V]: AlgoTestShapes_V,
-  [AlgoTestEnum.Obstacles_5_Basic]: AlgoTestObstacles_5_Basic,
-  [AlgoTestEnum.AlgoTestCollisionCheck_A]: AlgoTestCollisionCheck_A,
-  [AlgoTestEnum.AlgoTestCollisionCheck_B]: AlgoTestCollisionCheck_B,
-  [AlgoTestEnum.AlgoTestCollisionCheck_C]: AlgoTestCollisionCheck_C,
-  [AlgoTestEnum.AlgoTestOfficialMockLayout]: AlgoTestOfficialMockLayout,
+	[AlgoTestEnum.Custom]: AlgoTestCustom,
+	[AlgoTestEnum.Basic_Mock]: AlgoTestBasicMock,
+	[AlgoTestEnum.Basic_U_Turn]: AlgoTestBasicUTurn,
+	[AlgoTestEnum.Corners]: AlgoTestCorners,
+	[AlgoTestEnum.Obstacles_7_A]: AlgoTestObstacles_7_A,
+	[AlgoTestEnum.Obstacles_7_B]: AlgoTestObstacles_7_B,
+	[AlgoTestEnum.Shapes_V]: AlgoTestShapes_V,
+	[AlgoTestEnum.Obstacles_5_Basic]: AlgoTestObstacles_5_Basic,
+	[AlgoTestEnum.AlgoTestCollisionCheck_A]: AlgoTestCollisionCheck_A,
+	[AlgoTestEnum.AlgoTestCollisionCheck_B]: AlgoTestCollisionCheck_B,
+	[AlgoTestEnum.AlgoTestCollisionCheck_C]: AlgoTestCollisionCheck_C,
+	[AlgoTestEnum.AlgoTestOfficialMockLayout]: AlgoTestOfficialMockLayout,
 };
-
-// Specific Test Exports
-export { AlgoTestCustom } from "./custom";
-export { AlgoTestBasicMock } from "./basic_mock";
-export { AlgoTestBasicUTurn } from "./basic_u_turn";
-export { AlgoTestCorners } from "./corners";
-export { AlgoTestObstacles_7 } from "./obstacles_7";
-export { AlgoTestShapes_V } from "./shapes";
-export { AlgoTestObstacles_5_Basic } from "./obstacles_5";
-export {
-  AlgoTestCollisionCheck_A,
-  AlgoTestCollisionCheck_B,
-  AlgoTestCollisionCheck_C,
-} from "./collision_check";
-export { AlgoTestOfficialMockLayout } from "./official_mock_layout";
--- algo/simulator/src/tests/algorithm/obstacles_7.ts
+++ algo/simulator/src/tests/algorithm/obstacles_7.ts
@@ -1,16 +1,26 @@
 import { AlgoTestDataInterface } from ".";
-import { Obstacle, Direction } from "../../schemas/entity";
+import { Direction } from "../../schemas/entity";
 
-const obstacles: Obstacle[] = [
-  { id: 1, x: 1, y: 10, d: Direction.NORTH },
-  { id: 2, x: 9, y: 8, d: Direction.WEST },
-  { id: 3, x: 6, y: 1, d: Direction.EAST },
-  { id: 4, x: 1, y: 18, d: Direction.EAST },
-  { id: 5, x: 18, y: 18, d: Direction.SOUTH },
-  { id: 6, x: 18, y: 0, d: Direction.NORTH },
-  { id: 7, x: 12, y: 17, d: Direction.SOUTH },
-];
+export const AlgoTestObstacles_7_A: AlgoTestDataInterface = {
+	obstacles: [
+		{ id: 1, x: 1, y: 10, d: Direction.NORTH },
+		{ id: 2, x: 9, y: 8, d: Direction.WEST },
+		{ id: 3, x: 6, y: 1, d: Direction.EAST },
+		{ id: 4, x: 1, y: 18, d: Direction.EAST },
+		{ id: 5, x: 18, y: 18, d: Direction.SOUTH },
+		{ id: 6, x: 18, y: 0, d: Direction.NORTH },
+		{ id: 7, x: 12, y: 17, d: Direction.SOUTH },
+	],
+};
 
-export const AlgoTestObstacles_7: AlgoTestDataInterface = {
-  obstacles: obstacles,
+export const AlgoTestObstacles_7_B: AlgoTestDataInterface = {
+	obstacles: [
+		{ id: 1, x: 0, y: 17, d: Direction.EAST },
+		{ id: 2, x: 5, y: 12, d: Direction.SOUTH },
+		{ id: 3, x: 7, y: 5, d: Direction.NORTH },
+		{ id: 4, x: 15, y: 2, d: Direction.WEST },
+		{ id: 5, x: 11, y: 14, d: Direction.EAST },
+		{ id: 6, x: 16, y: 19, d: Direction.SOUTH },
+		{ id: 7, x: 19, y: 9, d: Direction.WEST },
+	],
 };
