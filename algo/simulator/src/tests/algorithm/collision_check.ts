9df0386738cf8f50cded6a06a42a90403183c9c7
kakapoot <angyujuan.2001@gmail.com>
2025-02-13 15:10:23 +0800

    sim fix: obstacle ID for collision test

--- algo/simulator/src/tests/algorithm/collision_check.ts
+++ algo/simulator/src/tests/algorithm/collision_check.ts
@@ -2,23 +2,23 @@ import { AlgoTestDataInterface } from ".";
 import { Direction } from "../../schemas/entity";
 
 export const AlgoTestCollisionCheck_A: AlgoTestDataInterface = {
-  obstacles: [
-    { id: 1, x: 1, y: 13, d: Direction.SOUTH },
-    { id: 2, x: 7, y: 13, d: Direction.WEST },
-  ],
+	obstacles: [
+		{ id: 1, x: 1, y: 13, d: Direction.SOUTH },
+		{ id: 2, x: 7, y: 13, d: Direction.WEST },
+	],
 };
 
 export const AlgoTestCollisionCheck_B: AlgoTestDataInterface = {
-  obstacles: [
-    { id: 1, x: 10, y: 15, d: Direction.SOUTH },
-    { id: 2, x: 10, y: 7, d: Direction.NORTH },
-  ],
+	obstacles: [
+		{ id: 1, x: 10, y: 15, d: Direction.SOUTH },
+		{ id: 2, x: 10, y: 7, d: Direction.NORTH },
+	],
 };
 
 export const AlgoTestCollisionCheck_C: AlgoTestDataInterface = {
-  obstacles: [
-    { id: 1, x: 9, y: 5, d: Direction.WEST },
-    { id: 2, x: 9, y: 9, d: Direction.EAST },
-    { id: 2, x: 17, y: 7, d: Direction.WEST },
-  ],
+	obstacles: [
+		{ id: 1, x: 9, y: 5, d: Direction.WEST },
+		{ id: 2, x: 9, y: 9, d: Direction.EAST },
+		{ id: 3, x: 17, y: 7, d: Direction.WEST },
+	],
 };
