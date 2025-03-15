be6987bf1331c9c672be74b99151282711666bde
HiIAmTzeKean <ngtzekean@gmail.com>
2025-03-15 12:19:40 +0800

    Add dependency to android

--- rpi/task2.py
+++ rpi/task2.py
@@ -8,10 +8,11 @@ from typing import Any, Optional
 import requests
 
 from .base_rpi import RaspberryPi
-from .communication.camera import snap_using_libcamera, snap_using_picamera2
+from .communication.android import AndroidMessage
+from .communication.camera import snap_using_picamera2
 from .communication.pi_action import PiAction
-from .constant.consts import Category, manual_commands, stm32_prefixes, FORWARD_SPEED
-from .constant.settings import URL
+from .constant.consts import FORWARD_SPEED, Category, manual_commands, stm32_prefixes
+from .constant.settings import URL, API_TIMEOUT
 
 
 logger = logging.getLogger(__name__)
@@ -30,13 +31,13 @@ action_list_first_right = [
     "half_left",
     "left_correct",
     "frontuntil",
-]            
+]
 action_list_second_left = [
     f"W{FORWARD_SPEED}|0|35",
-    "left", #robot 15cm apart from wall, 20cm turn radius
+    "left",  # robot 15cm apart from wall, 20cm turn radius
     f"R{FORWARD_SPEED}|0|30",
     "T30|58|183",
-    f"r{FORWARD_SPEED}|0|30", #15cm apart from wall on opposite side
+    f"r{FORWARD_SPEED}|0|30",  # 15cm apart from wall on opposite side
     f"R{FORWARD_SPEED}|0|30",
     "right",
     f"T{FORWARD_SPEED}|0|20",
@@ -67,6 +68,7 @@ action_list_second_right = [
     f"W{FORWARD_SPEED}|0|15",
 ]
 
+
 class TaskTwo(RaspberryPi):
     def __init__(self) -> None:
         super().__init__()
@@ -85,18 +87,32 @@ class TaskTwo(RaspberryPi):
             self.proc_recv_stm32 = Process(target=self.recv_stm)
             self.proc_command_follower = Process(target=self.command_follower)
             self.proc_rpi_action = Process(target=self.rpi_action)
-            self.unpause.set()
+            self.proc_android_controller = Process(target=self.android_controller)
+            self.proc_recv_android = Process(target=self.recv_android)
+            
             self.proc_recv_stm32.start()
             self.proc_command_follower.start()
             self.proc_rpi_action.start()
+            self.proc_android_controller.start()
+            self.proc_recv_android.start()
 
             self.set_actions(action_list_init)
+            self.unpause.set()
 
             logger.info("Child Processes started")
             # self.proc_android_controller.join()
-            self.proc_recv_stm32.join()
-            self.proc_command_follower.join()
-            self.proc_rpi_action.join()
+            # self.proc_recv_android.join()
+            # self.proc_recv_stm32.join()
+            # self.proc_rpi_action.join()
+            # self.proc_command_follower.join()
+            
+            processes = [getattr(self, attr) for attr in dir(self) if attr.startswith("proc_")]
+            for process in processes:
+                try:
+                    process.join()
+                except Exception as e:
+                    logger.error(f"Error joining process, likely already killed: {e}")
+            return
         except KeyboardInterrupt:
             self.stop()
 
@@ -125,12 +141,10 @@ class TaskTwo(RaspberryPi):
         """
         while True:
             action = self.rpi_action_queue.get()
-            logger.debug(
-                f"PiAction retrieved from queue: {action.cat} {action.value}")
+            logger.debug(f"PiAction retrieved from queue: {action.cat} {action.value}")
             if action.cat == Category.SNAP.value:
                 self.ready_snap.wait()
-                results = self.recognize_image(
-                    obstacle_id_with_signal=action.value)
+                results = self.recognize_image(obstacle_id_with_signal=action.value)
 
                 if self.first_obstacle:
                     self.first_obstacle = False
@@ -150,6 +164,94 @@ class TaskTwo(RaspberryPi):
             elif action.cat == Category.STITCH.value:
                 self.request_stitch()
 
+    def reconnect_android(self) -> None:
+        """Handles the reconnection to Android in the event of a lost connection."""
+        logger.info("Reconnection handler is watching...")
+
+        while True:
+            # Wait for android connection to drop
+            self.android_dropped.wait()
+
+            logger.error("Android is down")
+
+            # Kill child processes
+            logger.debug("Stopping android child processes")
+            self.proc_android_controller.kill()
+            self.proc_recv_android.kill()
+
+            # Wait for the child processes to finish
+            self.proc_android_controller.join()
+            self.proc_recv_android.join()
+            assert self.proc_android_controller.is_alive() is False
+            assert self.proc_recv_android.is_alive() is False
+            logger.debug("Android process stopped")
+
+            # Clean up old sockets
+            self.android_link.disconnect()
+            self.android_link.connect()
+
+            # Recreate Android processes
+            self.proc_recv_android = Process(target=self.recv_android)
+            self.proc_android_controller = Process(target=self.android_controller)
+            self.proc_recv_android.start()
+            self.proc_android_controller.start()
+
+            logger.info("Android processes restarted")
+            self.android_queue.put(AndroidMessage(Category.INFO.value, "You are reconnected!"))
+            self.android_queue.put(AndroidMessage(Category.MODE.value, "path"))
+
+            self.android_dropped.clear()
+    
+    def android_controller(self) -> None:
+        """
+        [Child process] Responsible for retrieving messages
+        from android_queue and sending them over the Android link.
+        """
+        while True:
+            try:
+                # blocks for 0.05 seconds to check if there are any messages
+                # in the queue
+                message = self.android_queue.get(timeout=0.05)
+                self.android_link.send(message)
+            except queue.Empty:
+                continue
+            except OSError:
+                self.android_dropped.set()
+                logger.error("OSError. Event set: Android dropped")
+            except Exception as e:
+                logger.error(f"Error sending message to Android: {e}")
+            
+    def recv_android(self) -> None:
+        """
+        [Child Process] Processes the messages received from Android
+        """
+        while True:
+            android_str: Optional[str] = None
+            try:
+                android_str = self.android_link.recv()
+            except OSError:
+                self.android_dropped.set()
+                logger.debug("OSError. Event set: Android dropped")
+
+            if android_str is None:
+                logger.debug("Empty message from andriod")
+                continue
+
+            message: dict = json.loads(android_str)
+
+            if message["cat"] == "control":
+                # Commencing path following
+                if not self.command_queue.empty():
+                    self.unpause.set()
+
+                    logger.info("Start command received, starting robot on path!")
+                    self.android_queue.put(AndroidMessage(Category.INFO.value, "Starting robot on path!"))
+                else:
+                    logger.warning("The command queue is empty, please set obstacles.")
+                    self.android_queue.put(
+                        AndroidMessage(Category.ERROR.value, "Command queue empty (no obstacles)")
+                    )
+
     def command_follower(self) -> None:
         """
         [Child Process]
@@ -164,26 +266,21 @@ class TaskTwo(RaspberryPi):
             if command.startswith(stm32_prefixes):
                 strings = str(command)
                 parts = strings.split("|")
-                self.outstanding_stm_instructions.set(
-                    self.outstanding_stm_instructions.get()+1)
-                self.stm_link.send_cmd(parts[0][0], int(
-                    parts[0][1:]), float(parts[1]), float(parts[2]))
+                self.outstanding_stm_instructions.set(self.outstanding_stm_instructions.get() + 1)
+                self.stm_link.send_cmd(parts[0][0], int(parts[0][1:]), float(parts[1]), float(parts[2]))
                 logger.debug(f"Sending to STM32: {command}")
 
             elif command.startswith("SNAP"):
                 obstacle_id_with_signal = command.replace("SNAP", "")
-                self.rpi_action_queue.put(
-                    PiAction(cat=Category.SNAP, value=obstacle_id_with_signal))
+                self.rpi_action_queue.put(PiAction(cat=Category.SNAP, value=obstacle_id_with_signal))
                 self.unpause.clear()
 
             elif command == Category.FIN.value:
-                logger.info(
-                    f"At FIN->self.current_location: {self.current_location}")
+                logger.info(f"At FIN->self.current_location: {self.current_location}")
                 self.unpause.clear()
                 logger.debug("unpause cleared")
                 logger.info("Commands queue finished.")
-                self.rpi_action_queue.put(
-                    PiAction(cat=Category.STITCH, value=""))
+                self.rpi_action_queue.put(PiAction(cat=Category.STITCH, value=""))
                 self.finish_all.wait()
                 self.finish_all.clear()
                 logger.debug("All processes up to stich finished")
@@ -202,18 +299,16 @@ class TaskTwo(RaspberryPi):
                 if message.startswith("f"):
                     logger.debug(f"stm finish {message}")
                     outstanding_stm_instructions = self.outstanding_stm_instructions.get()
+                    self.outstanding_stm_instructions.set(outstanding_stm_instructions - 1)
                     if outstanding_stm_instructions - 1 == 0:
                         self.ready_snap.set()
                         self.unpause.clear()
-                    self.outstanding_stm_instructions.set(
-                        outstanding_stm_instructions - 1)
                     logger.debug(f"stm finish {message}")
                     logger.info("Releasing movement lock.")
                 elif message.startswith("r"):
                     logger.debug(f"stm ack {message}")
                 else:
-                    logger.warning(
-                        f"Ignored unknown message from STM: {message}")
+                    logger.warning(f"Ignored unknown message from STM: {message}")
             except Exception as e:
                 logger.error(f"Error in recv_stm: {e}")
 
@@ -231,12 +326,9 @@ class TaskTwo(RaspberryPi):
         filename = f"/home/rpi21/cam/{int(time.time())}_{obstacle_id}_{signal}.jpg"
         filename_send = f"{int(time.time())}_{obstacle_id}_{signal}.jpg"
         results = snap_using_picamera2(
-            obstacle_id=obstacle_id,
-            signal=signal,
             filename=filename,
             filename_send=filename_send,
             url=url,
-            # auto_callibrate=False,
         )
         return results
 
@@ -245,12 +337,14 @@ class TaskTwo(RaspberryPi):
 
         if the API is down, an error message is sent to the Android
         """
-        response = requests.get(url=f"{URL}/stitch", timeout=2.0)
-
-        # TODO should retry if the response fails
-        if response.status_code != 200:
-            logger.error("Error when requesting stitch from the API.")
+        max_retries = 3
+        for attempt in range(max_retries):
+            response = requests.get(url=f"{URL}/stitch", timeout=API_TIMEOUT)
+            if response.status_code != 200:
+                logger.error(f"Error when requesting stitch from the API. Attempt {attempt + 1} of {max_retries}")
+                continue
+            logger.info("Images stitched!")
+            self.finish_all.set()
             return
 
-        logger.info("Images stitched!")
-        self.finish_all.set()
+        logger.error("Failed to request stitch from the API after multiple attempts.")
